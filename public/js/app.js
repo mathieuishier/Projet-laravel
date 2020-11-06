68352439049265558a770a8edb3c2e66b1b3a778
Thibault COSTA <8costa21@gmail.com>
2020-11-06 14:47:25 +0100

    xxx

--- public/js/app.js
+++ public/js/app.js
@@ -49899,8 +49899,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\wamp64\www\cours\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\wamp64\www\cours\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /Users/link/xdev/trello.ooo/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /Users/link/xdev/trello.ooo/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
